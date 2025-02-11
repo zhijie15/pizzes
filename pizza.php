bed9fd57aba3c863e47134b40c92d08663bdc8e2
zhijie15 <y1074554p@iespoblenou.org>
2025-02-11 11:10:07 +0000

    Commit 4 de l’aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -1,2 +1,31 @@
-Has demanat només la pizza bàsica. El preu és 9.68 € (IVA inclòs)<br>
-Autor Zhijie
+<html>
+ <head>
+ <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+ <title>FES LA TEVA PIZZA</title>
+ </head>
+ <body>
+ <?php
+ $preu = 8;
+ $IVA=21;
+ if (empty($_GET['ingr'])) {
+ $pvp=((100+$IVA)/100)*$preu;
+ echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
+ exit(0);
+ }
+ else {
+ $IVA=21;
+ $pvp=((100+$IVA)/100)*($preu+count($_GET['ingr']));
+ echo "Has demanat la pizza bàsica amb:<br> ";
+ foreach ($_GET['ingr'] as $ingredient) {
+ if ($ingredient) {
+ echo "* $ingredient<br>";
+ }
+ }
+ echo "El preu és $pvp € (IVA inclòs)<br>";
+ exit(0);
+}
+echo "<br>";
+echo "Autor: zhli"."<br>";
+ ?>
+ </body>
+</html>
